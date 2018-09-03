6f14fd928be9bf72ed884f8099dff43fb9766114
Syed Naim <syednaim@outlook.com>
2018-09-03 13:28:37 -0400

    Adjusted layout of Blog page, and displayed content.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import '../styles/Blog.css';
 
 class Blog extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blogArray: []
+        }
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('/blogs/');
+        console.log(res.data);
+        this.setState({
+            blogArray: res.data
+        });
+    }
+
+    renderBlogs = (blogArray) => {
+        const data = blogArray;
+        const listItems = data.map((d) =>
+        <div>
+            <hr/>
+            <h2>{d.title}</h2>
+            <h5><span className="glyphicon glyphicon-time"></span> Post by {d.author}, {d.date}.</h5>
+            <h5><span className="label label-danger">Book</span> <span className="label label-primary">Review</span></h5><br/>
+            <p className="contentP">{d.content}</p>
+            <br/>
+            <br/>
+        </div>
+        );
         return (
-            <div class="row" style={styles.bodyText}>
-                <div class="col-sm-2 text-center">
-                    <img src="bandmember.jpg" class="img-circle" height="65" width="65" alt="Avatar" />
-                </div>
-                <div class="col-sm-10">
-                    <h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>
-                    <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <br />
-                </div>
-                <div class="col-sm-2 text-center">
-                    <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar" />
-                </div>
-                <div class="col-sm-10">
-                    <h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>
-                    <p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <br />
-                    <p><span class="badge">1</span> Comment:</p><br />
-                    <div class="row">
-                        <div class="col-sm-2 text-center">
-                            <img src="bird.jpg" class="img-circle" height="65" width="65" alt="Avatar" />
-                        </div>
-                        <div class="col-xs-10">
-                            <h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>
-                            <p>Me too! WOW!</p>
-                            <br />
-                        </div>
-                    </div>
+            <div>
+                {listItems}
+            </div>
+        );
+    }
+
+    render() {
+        var blogArray = this.state.blogArray;
+
+        return (
+            <div id="blogBackground">
+                <br/>
+                <div>{/* <div className="col-sm-9"> */}
+                    <h4><small>RECENT POSTS</small></h4>
+                    {this.renderBlogs(blogArray)}
                 </div>
             </div>
         );
@@ -39,9 +55,3 @@ class Blog extends Component {
 }
 
 export default Blog;
-
-const styles = {
-    bodyText: {
-        color: "#FFFFFF"
-    }
-}
