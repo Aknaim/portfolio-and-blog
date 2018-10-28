cce3a05a75d060cd8f9ae4b9c0a742370811f5bf
Syed Naim <syednaim@outlook.com>
2018-10-27 22:02:19 -0400

    Layout change for contact page, with new image and email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,12 +4,15 @@ import '../styles/Contact.css';
 class Contact extends Component {
   render() {
     return (
-        <div id="contactBackground">
-          <div className = "div-contact">
-            <h1>Contact Me</h1>
-            <p>You can reach me at <a className="email" href="mailto:syednaim@outlook.com"> syednaim@outlook.com</a></p>
+        <div id="body">
+          <div class="container-fluid">
+            <img class="contactImg" src="http://richthinking.net/wp-content/uploads/2017/07/contact.jpg"/>          
+            <div className = "div-contact">
+              <h1 id="contactTitle">Contact Me</h1>
+              <p id="contactMsg">You can reach me at: <a className="email" href="mailto:syednaim@outlook.com"> syednaim@outlook.com</a></p>
+            </div>
           </div>
-        </div>
+          </div>
     );
   }
 }
